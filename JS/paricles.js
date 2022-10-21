970ab79bdf6094f582c077e2f98c82965b9b0b9c
Andrea.Bignardi <Andrea.Bignardi@wolterskluwer.com>
2022-10-21 18:02:12 +0200

    wip particles

---
+++ JS/paricles.js
@@ -0,0 +1,70 @@
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let particlesArray;
+
+let mouse = {
+  x: null,
+  y: null,
+  radius: (canvas.height / 80) * (canvas.width / 80),
+};
+
+window.addEventListener("mousemove", (event) => {
+  mouse.x = event.x;
+  mouse.y = event.y;
+});
+
+class Particle {
+  constructor(x, y, directionX, directionY, size, color) {
+    this.x = x;
+    this.y = y;
+    this.directionX = directionX;
+    this.directionY = directionY;
+    this.size = size;
+    this.color = color;
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
+    ctx.fillsStyle = "#a4b494";
+    ctx.fill();
+  }
+
+  update() {
+    if (this.x > canvas.width || this.x < 0) this.directionX = -this.directionX;
+    if (this.y > canvas.height || this.y < 0)
+      this.directionY = -this.directionY;
+
+    let dx = mouse.x - this.x;
+    let dy = mouse.y - this.y;
+    let distance = Math.sqrt(dx * dx + dy * dy);
+    if (distance < mouse.radius + this.size) {
+      this.x = randomMovement();
+      this.y = randomMovement();
+    }
+
+    this.x += this.directionX;
+    this.y += this.directionY;
+    this.draw();
+  }
+
+  randomMovement() {
+    min = -10;
+    max = 10;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+}
+
+function init() {
+  particlesArray = [];
+  let particlesNum = (canvas.height * canvas.width) / 9000;
+  for (let i = 0; i < particlesNum; i++) {
+    let size = Math.random() * 5 + 1;
+    let x = Math.random() * (innerWidth - size * 2 - size * 2) + size * 2;
+    let y = Math.random() * (innerHeight - size * 2 - size * 2) + size * 2;
+    let directionX = Math.random() * 5;
+  }
+}
